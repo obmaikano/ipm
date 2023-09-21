3902918065ad89b5f8adcfb0ef7d40966b1294c1
Obakeng Maikano <omaikano@dottess.co.bw>
2023-09-21 23:32:57 +0200

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { lazy, useEffect } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { themeChange } from 'theme-change'
+import checkAuth from './app/auth';
+import initializeApp from './app/init';
+
+// Importing pages
+const Login = lazy(() => import('./pages/Login'))
+
+// Initializing different libraries
+initializeApp()
+
+// Check for login and initialize axios
+const token = checkAuth()
 
 function App() {
+
+  useEffect(() => {
+    // 👆 daisy UI themes initialization
+    themeChange(false)
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/documentation" element={<Documentation />} />
+          
+          Place new routes over this 
+          <Route path="/app/*" element={<Layout />} /> */}
+
+          <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
+
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
